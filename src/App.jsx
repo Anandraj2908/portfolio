aea2e46a7bb49255aeb09a7917423d6795c86893
Anand Raj <anandraj.solutions@gmail.com>
2023-12-23 22:30:36 +0530

    social circle added

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
       <a href={"https://moviefreak.anandraj.tech"} ><button><h3>MovieFreak</h3></button></a>
       <a href={"https://reactredux.anandraj.tech"}><button><h3>React Redux</h3></button></a>
       <a href={"https://nirvanchat.anandraj.tech"}><button><h3>Nirvan Chat</h3></button></a>
+      <a href={"https://socialcircle.anandraj.tech"}><button><h3>Social Circle</h3></button></a>
       <p>Something Special comming soon</p>
     </>
   )
